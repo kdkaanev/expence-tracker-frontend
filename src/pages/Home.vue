eb97775c026a90e4186a892647b144707ae81f25
kdkaanev <kdkaane@github.io>
2025-09-03 22:56:06 +0300

    style homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -53,6 +53,23 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 </template>
 
 <style scoped>
+article {
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f5f5f5;
+}
+.container {
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 400px;
+  height: 90%;
+  text-align: center;
+}
 .page {
   width: 50px;
   height: 50px;
